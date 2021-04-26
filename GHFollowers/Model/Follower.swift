90326d0eef23f72918c452d0c9180c7f0c752251
JosephRTi <josephrt@yandex.ru>
2021-04-27 01:16:31 +0300

    New models were created

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Егор Уваров on 26.04.2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}

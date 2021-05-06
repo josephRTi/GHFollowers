aef3f9d61ecc0c16c8ad5df6c303b669890a7e6d
JosephRTi <josephrt@yandex.ru>
2021-05-06 12:45:45 +0300

    Added downloading avatars

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Егор Уваров on 27.04.2021.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+    private let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -75,3 +75,4 @@ class FollowerListVC: UIViewController {
         }
     }
 }
+

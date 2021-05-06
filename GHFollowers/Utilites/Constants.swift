8cf9092734bc346e5736b6ca79f6738246b96dcc
JosephRTi <josephrt@yandex.ru>
2021-05-06 21:19:16 +0300

    User info screen has been implemented

---
+++ GHFollowers/Utilites/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Егор Уваров on 06.05.2021.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+    static let repos = "folder"
+    static let gists = "text.alignleft"
+    static let followers = "heart"
+    static let following = "person.2"
+}
